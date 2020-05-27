22ba7a51ecf6e25c0b45b4e63a0d5f6490e5e5b1
VishwaMishra <vishwabeit20@gmail.com>
2020-05-27 11:34:04 +0530

    Authentication for signin is completed

--- InstagramClone/server/keys.js
+++ InstagramClone/server/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   MONGOURI:
     "mongodb+srv://Rajesh:WAzojUEJDBEtuqgw@cluster0-psgud.mongodb.net/test?retryWrites=true&w=majority",
+  JWT_SECRET:"efghiefghi44332"
 };
--- InstagramClone/server/routes/auth.js
+++ InstagramClone/server/routes/auth.js
@@ -3,9 +3,15 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../keys");
+const requireLogin = require("../middleware/requireLogin");
 
 router.get("/", (req, res) => {
   res.send("Hello");
+});
+router.get("/protected", requireLogin, (req, res) => {
+  res.send("Hello User");
 });
 
 router.post("/signup", (req, res) => {
@@ -26,7 +32,7 @@ router.post("/signup", (req, res) => {
         const user = new User({
           name,
           email,
-          password:hashedpassword,
+          password: hashedpassword,
         });
         user
           .save()
@@ -41,6 +47,32 @@ router.post("/signup", (req, res) => {
     .catch((err) => {
       console.log(err);
     });
+});
+
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(422).json({ error: "please filled email or password" });
+  }
+  User.findOne({ email: email }).then((savedUser) => {
+    if (!savedUser) {
+      return res.status(422).json({ error: "Invalid Email or password" });
+    }
+    bcrypt
+      .compare(password, savedUser.password)
+      .then((doMatch) => {
+        if (doMatch) {
+          //res.json({ message: "Successfully signed in" });
+          const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
+          res.json({ token });
+        } else {
+          return res.status(422).json({ error: "Invalid Email or password" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 });
 
 module.exports = router;

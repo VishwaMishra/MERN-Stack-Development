dd7f2d11ca58fb2b14cb230aa84d7479a72ec2b8
VishwaMishra <vishwabeit20@gmail.com>
2020-06-02 20:15:33 +0530

    User SignUp by posting Data completed.

--- InstagramClone/client1/src/component/Pages/signup.js
+++ InstagramClone/client1/src/component/Pages/signup.js
@@ -1,19 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 //import Signup from './../../../../client/src/component/Pages/signup';
 import { Link } from "react-router-dom";
+import M from "materialize-css";
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const PostData = () => {
+    fetch("http://localhost:5000/signup", {
+      mathod: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "",
+        password: "",
+        email: "",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          M.toast({ html: data.error });
+        }
+      });
+  };
+
   return (
     <div className="Login-Section">
       <div className="card">
         <h2>Instagram</h2>
-        <input type="text" placeholder="Name"></input>
-        <input type="text" placeholder="Email"></input>
-        <input type="password" placeholder="password"></input>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
         <button
           className="btn waves-effect waves-light "
           type="submit"
           name="action"
+          onClick={() => PostData}
         >
           Signup
           {/* <i class="material-icons right">send</i> */}
